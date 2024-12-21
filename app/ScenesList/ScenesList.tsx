ac96e7dcfbc22e8cdb7c0e8c098938b7ff3ad5a6
Kuba <qubatron15@gmail.com>
2024-12-21 23:10:16 +0100

    Listt component just added

--- app/ScenesList/ScenesList.tsx
+++ app/ScenesList/ScenesList.tsx
@@ -1,13 +1,34 @@
 import React, { FC } from 'react';
-import { Text } from 'react-native-paper';
+import { View } from 'react-native';
+import { List, Text } from 'react-native-paper';
 
-interface ScenesListProps {}
+interface ScenesListProps { }
 
-const ScenesList: FC<ScenesListProps> = () => (
-  <Text>
-    ScenesList Component updated
-  </Text>
-);
+const ScenesList: FC<ScenesListProps> = () => {
+  const [expanded, setExpanded] = React.useState(true);
+
+  const handlePress = () => setExpanded(!expanded);
+
+  return (
+    <View>
+      <List.Accordion
+        title="Uncontrolled Accordion - working?"
+        left={props => <List.Icon {...props} icon="folder" />}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+
+      <List.Accordion
+        title="Controlled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}
+        expanded={expanded}
+        onPress={handlePress}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+    </View>
+  );
+}
 
 export default ScenesList;
 
