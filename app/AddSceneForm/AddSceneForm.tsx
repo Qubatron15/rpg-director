31043afb6fea1af52261295ca38f4d8f1a723498
Kuba <qubatron15@gmail.com>
2024-12-28 22:37:49 +0100

    buttons added for the modal

--- app/AddSceneForm/AddSceneForm.tsx
+++ app/AddSceneForm/AddSceneForm.tsx
@@ -1,14 +1,56 @@
 import React, { FC } from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { Button, Text, TextInput } from 'react-native-paper';
 
+interface AddSceneFormProps { }
 
-interface AddSceneFormProps {}
+const AddSceneForm: FC<AddSceneFormProps> = () => {
+  const [name, setText] = React.useState("");
+  const [description, setDescription] = React.useState("");
 
-const AddSceneForm: FC<AddSceneFormProps> = () => (
-  <View>
-    <Text>AddSceneForm Component</Text>
-  </View>
-);
+  return (
+    <View>
+      <TextInput label="Name"
+        value={name}
+        onChangeText={text => setText(text)}
+        mode="outlined"
+      />
+      <TextInput label="Description"
+        value={description}
+        onChangeText={text => setDescription(text)}
+        mode="outlined"
+        multiline={true}
+      />
+      <View style={styles.buttonsContainer}>
+        <Button icon="camera"
+        mode="contained"
+        compact={true}
+        style={styles.button}
+          onPress={() => console.log('Pressed')}>
+          Add
+        </Button>
+        <Button icon="camera"
+        mode="contained"
+        compact={true}
+        style={styles.button}
+          onPress={() => console.log('Pressed')}>
+          Cancel
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  button: {
+    width: '50%',
+    // display: 'flex'
+  }
+});
 
 export default AddSceneForm;
