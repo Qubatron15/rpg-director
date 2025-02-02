f8fc196162a7d2956624586ed8b586c5d3b46d3e
Kuba <qubatron15@gmail.com>
2025-02-02 19:35:59 +0100

    config file introduced

--- app/store/slices/apiSlice.ts
+++ app/store/slices/apiSlice.ts
@@ -2,10 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const mongoDbApi = createApi({
     reducerPath: 'mongoDbApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com' }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://us-east-2.aws.neurelo.com/rest',
+        prepareHeaders: (headers, { getState }) => {
+            headers.set('X-API-KEY', '')
+            return headers
+        }
+    }),
     endpoints: (builder) => ({
         getAllScenes: builder.query({
-            query: () => 'products'
+            query: () => 'scenes',
         })
     })
 });
