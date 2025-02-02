f717aa3562c538ce7bfe6ba8ed7e371eba93cce7
Kuba <qubatron15@gmail.com>
2025-02-02 16:20:24 +0100

    unused files removed

--- app/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-// // TODO - possibly to remove for now
-// import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-// import { RootState, AppDispatch } from './store';
-
-// export const useAppDispatch: () => AppDispatch = useDispatch;
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
