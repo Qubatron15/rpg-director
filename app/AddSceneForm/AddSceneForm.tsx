501734017626fa7fb43f176d6d3511188873de9b
Kuba <qubatron15@gmail.com>
2025-02-09 20:12:51 +0100

    Progress bars + can open edit scene

--- app/AddSceneForm/AddSceneForm.tsx
+++ app/AddSceneForm/AddSceneForm.tsx
@@ -1,22 +1,36 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Button, HelperText, TextInput } from 'react-native-paper';
-import { useDispatch } from 'react-redux';
-import { addScene, SceneData } from '../store/slices/scenesListSlice';
-import { useNavigation } from '@react-navigation/native';
+import { Button, HelperText, TextInput, Text, ProgressBar } from 'react-native-paper';
+import { useDispatch, useSelector } from 'react-redux';
+import { addScene, getSceneById, SceneData } from '../store/slices/scenesListSlice';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { useAddNewSceneMutation } from '../store/slices/apiSlice';
+import { RootState } from '../store/store';
 
 const AddSceneForm: React.FC = () => {
   const dispatch = useDispatch();
   const navigation = useNavigation();
+  const route = useRoute();
 
-  const [formValues, setFormValues] = useState({ sceneName: '', sceneDescription: '' });
+  const sceneId = (route.params as { sceneId: string }).sceneId;
+  let selectedSceneData: SceneData | undefined;
+  if (sceneId) {
+    selectedSceneData = useSelector((state: RootState[]) => getSceneById(state, sceneId));
+  }
+
+  const [formValues, setFormValues] = useState({
+    sceneName: selectedSceneData?.name ?? '',
+    sceneDescription: selectedSceneData?.description ?? ''
+  });
   const [formDirty, setFormDirty] = useState(false);
   const [formValid, setFormValid] = useState(false);
 
   const [requestAddNewScene, { isLoading }] = useAddNewSceneMutation();
 
-  useEffect(() => setFormValid(!!(formDirty && formValues.sceneName)), [formDirty, formValues])
+  useEffect(
+    () => setFormValid(!!(formDirty && formValues.sceneName && !isLoading)),
+    [formDirty, formValues, isLoading]
+  );
 
   const handleTextChange = useCallback((fieldName: string, newValue: string) => {
     setFormValues(prevState => ({
@@ -55,6 +69,8 @@ const AddSceneForm: React.FC = () => {
 
   return (
     <View style={styles.formContainer}>
+      {isLoading && <ProgressBar indeterminate={true} />}
+
       <TextInput
         label="Name"
         value={formValues.sceneName}
--- app/ScenesList/ScenesListItem.tsx
+++ app/ScenesList/ScenesListItem.tsx
@@ -1,10 +1,11 @@
 import React, { useCallback, useState } from "react";
-import { Menu, Text } from "react-native-paper";
+import { Menu, ProgressBar, Text } from "react-native-paper";
 import { useDispatch } from "react-redux";
 import { deleteScene, SceneData } from "../store/slices/scenesListSlice";
 import SceneTile from "./SceneTile";
 import { useNavigation } from "expo-router";
 import { useDeleteSceneMutation } from "../store/slices/apiSlice";
+import { View } from "react-native";
 
 interface ScenesListItemProps {
     sceneData: SceneData,
@@ -31,9 +32,14 @@ const ScenesListItem: React.FC<ScenesListItemProps> = ({ sceneData }: ScenesList
         navigation.navigate('ViewScene/ViewScene', { sceneId: sceneData.id });
     }, []);
 
+    const handleEditScene = useCallback(() => {
+        setMenuVisible(false);
+        navigation.navigate('AddSceneForm/AddSceneForm', { sceneId: sceneData.id });
+    }, []);
+
     const handleDeleteScene = useCallback(async () => {
         try {
-            // TODO - add a spinner here
+            // TODO - spinner is not visible when loading
             await requestDeleteScene(sceneData.id).unwrap();
             dispatch(deleteScene(sceneData));
         } catch (error) {
@@ -42,18 +48,22 @@ const ScenesListItem: React.FC<ScenesListItemProps> = ({ sceneData }: ScenesList
     }, []);
 
     return (
-        <Menu
-            visible={menuVisible}
-            onDismiss={handleCloseMenu}
-            anchor={<Text onPress={handleViewScene} onLongPress={handleOpenMenu}><SceneTile sceneData={sceneData} /></Text>}
-        >
+        <View>
+            {isLoading && <ProgressBar indeterminate={true} />}
 
-            <Menu.Item onPress={handleViewScene} title="View" />
-            <Menu.Item onPress={() => { }} title="Edit" />
-            <Menu.Item onPress={handleDeleteScene} title="Delete" />
-            <Menu.Item onPress={() => { }} title="IDK - move?" />
+            <Menu
+                visible={menuVisible}
+                onDismiss={handleCloseMenu}
+                anchor={<Text onPress={handleViewScene} onLongPress={handleOpenMenu}><SceneTile sceneData={sceneData} /></Text>}
+            >
 
-        </Menu>
+                <Menu.Item onPress={handleViewScene} title="View" />
+                <Menu.Item onPress={handleEditScene} title="Edit" />
+                <Menu.Item onPress={handleDeleteScene} title="Delete" />
+                <Menu.Item onPress={() => { }} title="IDK - move?" />
+
+            </Menu>
+        </View>
     )
 }
 
