2469e9070fd7a8f20d977c946763cd7662584098
Kuba <qubatron15@gmail.com>
2025-02-15 21:57:28 +0100

    loading spinner displayed in the middle

--- app/index.tsx
+++ app/index.tsx
@@ -5,20 +5,29 @@ import { store } from './store/store';
 import App from './app';
 import { initScenesList } from './store/slices/scenesListSlice';
 import { useGetAllScenesQuery } from './store/slices/apiSlice';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 const Index = () => {
   const dispatch = useDispatch();
-  const { data, error, isLoading } = useGetAllScenesQuery('');
-  console.log('-0--------', data, error, isLoading);
+  const { data, error, isLoading } = useGetAllScenesQuery(''); // TODO - add error handling here
 
   useEffect(() => { dispatch(initScenesList(data?.data ?? [])); }, [isLoading]);
 
   return (
-    <View style={{ flex: 1 }}>
-      {isLoading ? <ActivityIndicator size="large" animating={true} /> : <App />}
+    <View style={styles.view}>
+      {isLoading ? <ActivityIndicator size="large" animating={true} style={styles.spinner}/> : <App />}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1
+  },
+  spinner: {
+    flex: 1,
+    verticalAlign: 'middle'
+  }
+});
 
 export default Index;
